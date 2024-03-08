284f328286643446b1b5ee4e183d9a707b2033a4
Marouane <aittalebmarouane72@gmail.com>
2024-03-08 21:08:45 +0100

    exercie2 commit

--- src/TP2/EX4.java
+++ src/TP2/EX4.java
@@ -2,18 +2,22 @@ package TP2;
 
 import java.util.Scanner;
 
+
+    import java.util.Scanner;
+
 public class EX4 {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Entrez la valeur de X : ");
-        int x = scanner.nextInt();
+            System.out.print("Entrez la valeur de X : ");
+            int x = scanner.nextInt();
 
-        System.out.print("Entrez la valeur de N : ");
-        int n = scanner.nextInt();
+            System.out.print("Entrez la valeur de N : ");
+            int n = scanner.nextInt();
 
-        int resultat = (int) Math.pow(x, n);
-        System.out.println(x + "^" + n + " = " + resultat);
+            int resultat = (int) Math.pow(x, n);
+            System.out.println(x + "^" + n + " = " + resultat);
 
+        }
     }
-}
+
